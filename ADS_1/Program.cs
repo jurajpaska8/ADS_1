4e2a1fcfa93d9d07ea211880bc555b54c31148bd
juraj.paska <juraj.paska@siemens-healthineers.com>
2021-03-17 21:35:50 +0100

    test(DictionaryHandler): add helper code to test methods

--- ADS_1/Program.cs
+++ ADS_1/Program.cs
@@ -14,10 +14,37 @@ namespace ADS_1
             string[] lines = fileHandler.ReadLines();
             // transform lines into <string, int> dictionary
             Dictionary<string, int> dic = fileHandler.TransformLinesIntoDic(lines);
-            Dictionary<string, int> dicOver50k = fileHandler.GetWordsWithFrequentionHigherThanFreq(new Dictionary<string, int>(dic), 50_000);
-            Console.WriteLine(lines.Length);
-            Console.WriteLine(dic.Count);
-            Console.WriteLine(dicOver50k.Count);
+            // get words with freq. higher than 50k
+            Dictionary<string, int> dicOver50k = DictionaryHandler.GetWordsWithFrequentionHigherThanFreq(new Dictionary<string, int>(dic), 50_000);
+            //get original Dictionary as sorted one
+            var sortedDic = new SortedDictionary<string, int>(dic);
+            //get shrunked Dictionary as sorted one
+            var sortedDicOver50k = new SortedDictionary<string, int>(dicOver50k);
+
+            // return sum of all frequencies in over 50k dictionary
+            var freqSumSortedOver50k = DictionaryHandler.SumOfValues(sortedDicOver50k);
+            var freqSumOver50k = DictionaryHandler.SumOfValues(dicOver50k);
+
+            // return sum of all frequencies in over 50k dictionary
+            var freqSumSortedOriginal = DictionaryHandler.SumOfValues(sortedDic);
+            var freqSumOriginal = DictionaryHandler.SumOfValues(dic);
+
+
+            // freq of the
+            double freq_the = DictionaryHandler.GetRelativeFrequencyOfWord("the", sortedDicOver50k, freqSumSortedOver50k);
+            double freq_108 = DictionaryHandler.GetRelativeFrequencyOfWord(108, sortedDicOver50k, freqSumSortedOver50k);
+
+            // test gap frequencies
+            double q_all_sorted = DictionaryHandler.GetRelativeFrequencyOfGap(-1, sortedDicOver50k.Count, sortedDicOver50k, freqSumSortedOver50k);
+
+            double q_all_sorted_1_n = DictionaryHandler.GetRelativeFrequencyOfGap(0, sortedDicOver50k.Count - 1, sortedDicOver50k, freqSumSortedOver50k);
+            double q_all_sorted_words = DictionaryHandler.GetRelativeFrequencyOfGap("a", "your", sortedDicOver50k, freqSumSortedOver50k);
+
+            //print stats
+            Console.WriteLine(freq_the);
+            Console.WriteLine("Lines in file: " + lines.Length);
+            Console.WriteLine("KeyPairs in original dic: " + dic.Count);
+            Console.WriteLine("KeyPairs in shrunked dic: " + dicOver50k.Count);
         }
     }
 }
