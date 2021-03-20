e6c4dd0f422dcad9391c79c298aff5b9ebee355c
juraj.paska <juraj.paska@siemens-healthineers.com>
2021-03-20 21:06:41 +0100

    feat(BinaryTree): add basic data type with build from order method

---
+++ ADS_1/code/BinaryTreeFinal.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace ADS_1.code
+{
+    public class Node 
+    { 
+        public Node LeftNode { get; set; }
+        public Node RightNode { get; set; }
+        public string Word = "";
+        public double freq = double.NegativeInfinity;
+        public int order = -1;
+    };
+
+    class BinaryTreeFinal
+    {
+        public Node Root { get; set; }
+
+        public bool Add(string value, int order = -1)
+        {
+            Node before = null, after = this.Root;
+
+            while (after != null)
+            {
+                before = after;
+                if (string.Compare(value, after.Word) < 0) //Is new node in left tree? 
+                    after = after.LeftNode;
+                else if (string.Compare(value, after.Word) > 0) //Is new node in right tree?
+                    after = after.RightNode;
+                else
+                {
+                    //Exist same value
+                    return false;
+                }
+            }
+
+            Node newNode = new Node();
+            newNode.Word = value;
+            newNode.order = order;
+
+            if (this.Root == null)//Tree ise empty
+                this.Root = newNode;
+            else
+            {
+                if (string.Compare(value, before.Word) < 0)
+                    before.LeftNode = newNode;
+                else
+                    before.RightNode = newNode;
+            }
+
+            return true;
+        }
+
+        public void BuildFromOrder(string[] keys, int[] keysOrder)
+        {
+            if (keys.Length != keysOrder.Length)
+            {
+                Console.WriteLine("Bad inputs in BuildFromOrder");
+                return;
+            }
+            for(int i = 0; i < keysOrder.Length; i++)
+            {
+                Add(keys[keysOrder[i]], keysOrder[i]);
+            }
+        }
+    }
+}
